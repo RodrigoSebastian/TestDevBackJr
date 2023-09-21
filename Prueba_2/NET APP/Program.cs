02a0b7e7700bd3a234609be7c96722018ae030ff
Rodrigo Sebastián De la Rosa Andrés <rodrisebas-delarosa@hotmail.com>
2023-09-20 18:57:32 -0600

    Ex 1-2

--- Prueba_2/NET APP/Program.cs
+++ Prueba_2/NET APP/Program.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using System.Text;
+using MySql.Data.MySqlClient;
 
 namespace Prueba2 {
    class Program {
@@ -7,6 +8,7 @@ namespace Prueba2 {
          DatabaseConnectionPool pool = new DatabaseConnectionPool("SERVER=localhost;DATABASE=nuxiba;USERNAME=root;PASSWORD=;", 5);
 
          ListUsers(pool);
+         GenerateCSV(pool);
       }
 
       // Listar top 10 usuarios de la base antes creada (10 puntos)
@@ -18,14 +20,48 @@ namespace Prueba2 {
             
             MySqlDataReader reader = command.ExecuteReader();
 
+            Console.WriteLine("Lista de usuarios");
             while (reader.Read()) {
-               Console.WriteLine(reader.GetString(0) + " " + reader.GetString(1));
+               Console.WriteLine($"ID: {reader.GetInt32("userID")} | Nombre: {reader.GetString("nombre")} | Paterno: {reader.GetString("paterno")} | Materno: {reader.GetString("materno")}");
             }
+
+            reader.Close();
          } finally {
             pool.ReleaseConnecction(connection);
          }
       }
+
       // Generar un archivo csv con las siguienets campos con su información(Login, Nombre completo, sueldo, fecha Ingreso) (25 puntos)
+      private static void GenerateCSV(DatabaseConnectionPool pool)
+      {
+         MySqlConnection connection = pool.GetConnection();
+         try {
+            Console.WriteLine("\nGerando CSV");
+
+            string query = "SELECT * FROM usuarios AS us INNER JOIN empleados AS em ON us.userID = em.userID;";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            
+            MySqlDataReader reader = command.ExecuteReader();
+            var csv = new StringBuilder();
+            csv.AppendLine("Login,Nombre Completo,Sueldo,Fecha Ingreso");
+
+            while (reader.Read()) {
+               string login = reader.GetString("login");
+               string nombre = reader.GetString("nombre");
+               string paterno = reader.GetString("paterno");
+               string materno = reader.GetString("materno");
+               string sueldo = reader.GetString("sueldo");
+               string fechaIngreso = reader.GetString("fechaIngreso");
+
+               csv.Append($"{login},{nombre} {paterno} {materno.Trim()},{sueldo},{fechaIngreso}\n");
+            }
+            File.WriteAllText("C:/Users/rodri/Documents/Personal/Nuxiba/testdevbackjr/Prueba_2/NET APP/info.csv", csv.ToString());
+            reader.Close();
+            Console.WriteLine("CSV generado");
+         } finally {
+            pool.ReleaseConnecction(connection);
+         }
+      }
 
       // Poder actualizar el salario del algun usuario especifico (10 puntos)
       // Poder Tener una opcion para agregar un nuevo usuario y se pueda asiganar el salario y la fecha de ingreso por default el dia de hoy (25 puntos)
