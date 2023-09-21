76d15ff1bc91fc52f5cb616e47c4d1985a0c9ecd
Rodrigo Sebastián De la Rosa Andrés <rodrisebas-delarosa@hotmail.com>
2023-09-20 18:25:35 -0600

    DataBase connection with pool

---
+++ Prueba_2/NET APP/DatabaseConnectionPool.cs
@@ -0,0 +1,45 @@
+using MySql.Data.MySqlClient;
+
+// Se implemento el patron de diseño Object Pooling, para tener un mejor control de la cantidad sobre
+// la cantidad de conexiones que se pueden realizar a la base de datos  -> Enfoque multihilo
+// Tambien podria implementarse SINGLETON para tener una sola instancia de la conexion a la base de datos -> Enfoque solo un hilo
+namespace Prueba2 {
+   public class DatabaseConnectionPool
+   {
+      private Queue<MySqlConnection> pool;
+      private int maxPoolSize;
+
+      public DatabaseConnectionPool(string connectionString, int maxPoolSize) {
+         this.maxPoolSize = maxPoolSize;
+         this.pool = new Queue<MySqlConnection>(maxPoolSize);
+         for (int i = 0; i < maxPoolSize; i++) {
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            pool.Enqueue(connection);
+         }
+      }      
+
+      public MySqlConnection GetConnection()
+      {
+         lock (pool) {
+            if (pool.Count > 0) {
+               MySqlConnection connection = pool.Dequeue();
+               connection.Open();
+               return connection;
+            } else {
+               throw new InvalidOperationException("No existen conexiones disponibles");
+            }
+         }
+      }
+
+      public void ReleaseConnecction(MySqlConnection connection) {
+         lock (pool) {
+            if (pool.Count < maxPoolSize) {
+               connection.Close();
+               pool.Enqueue(connection);
+            } else {
+                connection.Dispose();
+            }
+         }
+      }
+   }
+}
--- Prueba_2/NET APP/Program.cs
+++ Prueba_2/NET APP/Program.cs
@@ -1,9 +1,34 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
 
 namespace Prueba2 {
    class Program {
+
       static void Main(string[] args) {
-         Console.WriteLine("Hello world");
+         DatabaseConnectionPool pool = new DatabaseConnectionPool("SERVER=localhost;DATABASE=nuxiba;USERNAME=root;PASSWORD=;", 5);
+
+         ListUsers(pool);
       }
+
+      // Listar top 10 usuarios de la base antes creada (10 puntos)
+      private static void ListUsers(DatabaseConnectionPool pool) {
+         MySqlConnection connection = pool.GetConnection();
+         try {
+            string query = "SELECT * FROM usuarios LIMIT 10";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            
+            MySqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read()) {
+               Console.WriteLine(reader.GetString(0) + " " + reader.GetString(1));
+            }
+         } finally {
+            pool.ReleaseConnecction(connection);
+         }
+      }
+      // Generar un archivo csv con las siguienets campos con su información(Login, Nombre completo, sueldo, fecha Ingreso) (25 puntos)
+
+      // Poder actualizar el salario del algun usuario especifico (10 puntos)
+      // Poder Tener una opcion para agregar un nuevo usuario y se pueda asiganar el salario y la fecha de ingreso por default el dia de hoy (25 puntos)
+      // PLUS: Si conoces algún patrón de diseño de software no dudes en usarlo (+ 10 puntos)
    }
 }
